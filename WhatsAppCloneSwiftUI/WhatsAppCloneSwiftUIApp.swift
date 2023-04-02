3428d2b16cfa51098b9a6017cb8760ae1cc78b4c
mkadr5 <muhammetkadir52@gmail.com>
2023-04-02 13:23:32 +0300

    Initial Commit

---
+++ WhatsAppCloneSwiftUI/WhatsAppCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppCloneSwiftUIApp.swift
+//  WhatsAppCloneSwiftUI
+//
+//  Created by Muhammet Kadir on 2.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
